36b7a92a458ccfb66d11854da1de47e081736ce6
Данила Богославцев <da.png@yandex.ru>
2021-08-22 19:36:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import re
+
+
+def valid_number():
+    print("Valid number pattern\n+7 (111) 111 11-11\nВведите номер: ")
+    number = str(input())
+    check_num = re.search(r'(\+7\s[\(]\d\d\d[\)]\s\d\d\d\s\d\d[-]\d\d)', number)
+    if check_num is None:
+        print(False, number)
+    else:
+        print(True, number)
+
+
+valid_number()
+
+#
+# valid number pattern
+# +7 (111) 111 11-11
+#
