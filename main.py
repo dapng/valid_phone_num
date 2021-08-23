43f24a1a698d3d0e73d180900b9c83db6a36f795
Данила Богославцев <da.png@yandex.ru>
2021-08-23 23:35:03 +0300

    Changed number validation and new regex

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import re
 
-
-def valid_number():
-    print("Valid number pattern\n+7 (111) 111 11-11\nВведите номер: ")
-    number = str(input())
-    check_num = re.search(r'(\+7\s[\(]\d\d\d[\)]\s\d\d\d\s\d\d[-]\d\d)', number)
-    if check_num is None:
-        print(False)
-    else:
-        print(True)
+# The first option
+def verify_number(number):
+    print(bool(re.search(r'^([\+][7]\s[\(]\d{3,3}[\)]\s\d{3,3}\s\d{2,2}[-]\d{2,2})$', number)))
 
 
-valid_number()
+verify_number("+7 (111) 111 11-11")
 
-
-
+# The second option
+# def verify_number(number):
+#     num = (re.search(r'([\+][7]\s[\(]\d{3,3}[\)]\s\d{3,3}\s\d{2,2}[-]\d{2,2})', number) and True)
+#     print(bool(num))
 #
-# valid number pattern
-# +7 (111) 111 11-11
 #
+# verify_number("+7 (111) 111 11-11")
