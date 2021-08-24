0a86807815c568c6c288e0c1c787a59c7999abd4
Данила Богославцев <da.png@yandex.ru>
2021-08-24 08:46:11 +0300

    fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ verify_number("+7 (111) 111 11-11")
 
 # The second option
 # def verify_number(number):
-#     num = (re.search(r'([\+][7]\s[\(]\d{3,3}[\)]\s\d{3,3}\s\d{2,2}[-]\d{2,2})', number) and True)
+#     num = (re.search(r'^([\+][7]\s[\(]\d{3,3}[\)]\s\d{3,3}\s\d{2,2}[-]\d{2,2})$', number) and True)
 #     print(bool(num))
 #
 #
